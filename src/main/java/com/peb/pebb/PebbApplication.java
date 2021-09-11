ab4f442fbb1d11f117cec9d81a194baad3991932
jmatondo <j_matondo@hotmail.com>
2021-09-11 08:44:40 +0200

    first commit1

--- src/main/java/com/peb/pebb/PebbApplication.java
+++ src/main/java/com/peb/pebb/PebbApplication.java
@@ -1,7 +1,13 @@
 package com.peb.pebb;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class PebbApplication {
@@ -10,4 +16,16 @@ public class PebbApplication {
 		SpringApplication.run(PebbApplication.class, args);
 	}
 
+	@Scheduled(initialDelay = 2000L, fixedDelayString = "${someJob.delay}") // toutesles 2 secondes
+	void someJob() throws Exception {
+		System.out.println("Now is := " + new Date());
+		Thread.sleep(1000L);
+	}
+
+}
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
+class scheduleConfig {
 }
