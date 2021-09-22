01e5e1c55aea4c20a66c55201ce4345117761407
jmatondo <j_matondo@hotmail.com>
2021-09-22 21:25:19 +0200

    first commit1

--- src/main/java/com/peb/pebb/PebbApplication.java
+++ src/main/java/com/peb/pebb/PebbApplication.java
@@ -12,11 +12,12 @@ import com.peb.pebb.role.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/* import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled; */
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PebbApplication {
@@ -44,21 +45,27 @@ public class PebbApplication {
 			 * appUserService.addRoleToUser("jmatondo@hotmail.com", "ROLE_ADMIN");
 			 */
 
+			RestTemplate restTemplate = new RestTemplate();
+			// Resume resume =
+			// restTemplate.getForObject("http://localhost:9050/peb/resumes/1",
+			// Resume.class);
+			String msg = restTemplate.getForObject("https://pebicc.herokuapp.com/wakeup", String.class);
+			System.out.println("Resume msg:= " + msg);
+
 		};
 	}
 
-	/*
-	 * @Scheduled(initialDelay = 2000L, fixedDelayString = "${someJob.delay}") //
-	 * toutesles 2 secondes void someJob() throws Exception {
-	 * System.out.println("Now is := " + new Date()); Thread.sleep(1000L); }
-	 */
+	@Scheduled(initialDelay = 2000L, fixedDelayString = "${someJob.delay}") // toutesles 2 secondes
+	void someJob() throws Exception {
+		// System.out.println("Now is := " + new Date()); Thread.sleep(1000L);
+	}
+
 }
 
-/*
- * @Configuration
- * 
- * @EnableScheduling
- * 
- * @ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
- * class scheduleConfig { }
- */
+@Configuration
+
+@EnableScheduling
+
+@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
+class scheduleConfig {
+}
